5f5aab7970a6bb152aad0ffc1ca339577209b6c8
zzx <2792133937@qq.com>
2020-11-07 11:28:26 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <div>23</div>
+    <router-view />
   </div>
 </template>
 
