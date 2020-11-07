c4bd101af5692405ef179f8813282605668d8211
itheima <916158721@qq.com>
2020-11-07 17:17:31 +0800

    m

---
+++ src/views/My/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="head"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.head {
+  background-color: #3f51b5;
+}
+</style>
