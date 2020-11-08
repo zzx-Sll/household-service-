0e18585f0466def5bf1fbb641bf8fce8557fea67
Twistzz <www.616058286@qq.com>
2020-11-08 20:21:53 +0800

    我的关注

---
+++ src/views/myfocus/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="myfocus">
+    <!-- 导航栏 -->
+    <van-nav-bar
+      :border="false"
+      class="page-nav-bar"
+      left-text="个人资料"
+      left-arrow
+      @click-left="$router.back()"
+    >
+
+</van-nav-bar>
+<van-grid direction="horizontal" :column-num="2" :border="false">
+  <van-grid-item  text="家服公司" @click="flag=true"/>
+  <van-grid-item  text="家服人员" @click="flag=false"/>
+</van-grid>
+
+<focus-company v-if="flag"></focus-company>
+
+<focus-worker v-else></focus-worker>
+  </div>
+</template>
+
+<script>
+import FocusWorker from './components/focus-woker'
+import FocusCompany from './components/focus-company'
+export default {
+  data () {
+    return {
+      flag: true
+    }
+  },
+  components: {
+    FocusWorker,
+    FocusCompany
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+.page-nav-bar{
+    height: 128px;
+
+    background-color:#3F51B5 ;
+
+  }
+  ::v-deep .van-grid-item__content{
+    color: white;
+    background-color: #3F51B5;
+
+  }
+  ::v-deep .van-nav-bar__text {
+    color: white;
+    font-size: 32px;
+  }
+  ::v-deep .van-grid-item__text {
+    color: white;
+    font-size: 28px;
+  }
+  ::v-deep .van-icon {
+    color: white;
+  }
+
+</style>
