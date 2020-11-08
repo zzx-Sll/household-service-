32df116b9e40a9330932b849c56e221f5c7c5da2
itheima <916158721@qq.com>
2020-11-08 14:54:46 +0800

    完成了首页的样式

--- src/views/layout/Home/index.vue
+++ src/views/layout/Home/index.vue
@@ -2,7 +2,8 @@
   <div class="all">
     <van-nav-bar left-text="家政平台"
                  left-arrow
-                 @click-left="onClickLeft" />
+                 @click-left="onClickLeft"
+                 class="navbar" />
     <div class="head">
       <!-- 上轮播图 -->
       <div class="swipe">
@@ -29,51 +30,8 @@
     <!-- 服务项目 -->
     <div class="content2">
       <ul>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-          </div>
-        </li>
-        <li>
-          <div class="box">
-            <div class="radiu"></div>
-            <span>nihao</span>
-          </div>
-        </li>
-        <li>
+        <li v-for="(item,index) in 8"
+            :key="index">
           <div class="box">
             <div class="radiu"></div>
             <span>nihao</span>
@@ -81,10 +39,73 @@
         </li>
       </ul>
     </div>
-    <van-cell icon="shop-o"
-              size="large">
-      <div slot="title">湖南家信认证家服公司</div>
-    </van-cell>
+    <!-- 公司中 -->
+    <div class="companydesign">
+      <van-cell icon="shop-o"
+                size="large">
+        <div slot="title">湖南家信认证家服公司</div>
+      </van-cell>
+
+      <van-cell v-for="(data,i) in 3"
+                :key="i">
+        <div class="comment_list">
+          <div class="touxiang">
+            <img src="https://img.yzcdn.cn/vant/apple-1.jpg"
+                 alt="">
+            <div>
+              <van-icon name="location"
+                        class="dizhi" />10000km
+            </div>
+          </div>
+          <div class="youall">
+            <div class="nameicon">
+              <div>公司名称</div>
+            </div>
+            <div>评价</div>
+            <div>标签</div>
+            <div>具体地址</div>
+          </div>
+        </div>
+      </van-cell>
+      <div class="gengduo">更多公司
+        <van-icon name="arrow" />
+      </div>
+    </div>
+
+    <!-- 服务人员详情 -->
+    <div class="companydesign">
+      <van-cell icon="manager"
+                size="large">
+        <div slot="title">诚信注册家服员</div>
+      </van-cell>
+
+      <van-cell v-for="(data,i) in 3"
+                :key="i">
+        <div class="comment_list">
+          <div class="touxiang">
+            <img src="https://img.yzcdn.cn/vant/apple-1.jpg"
+                 alt="">
+
+          </div>
+          <div class="youall">
+            <div class="nameicon">
+              <div>卢本伟</div>
+              <div>8000元/26天</div>
+            </div>
+            <div class="skil">
+              <span class="peoplegrid">八倍sks</span>
+              <span class="peoplegrid">10年老师傅</span>
+
+            </div>
+            <div class="myself"><span>长沙</span> | <span>18岁</span> | <span>干了18次</span> | <span>9条评价</span></div>
+
+          </div>
+        </div>
+      </van-cell>
+      <div class="gengduo">更多家服员
+        <van-icon name="arrow" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -110,7 +131,14 @@ export default {
 
 <style lang="scss" scoped>
 .all {
-  margin-bottom: 100px;
+  margin-top: 92px;
+  margin-bottom: 110px;
+  .navbar {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    bottom: 0;
+  }
 }
 .van-nav-bar {
   background-color: #3f51b5;
@@ -119,7 +147,7 @@ export default {
   color: white;
   font-size: 32px;
 }
-::v-deep .van-icon {
+::v-deep .van-nav-bar__arrow {
   color: white;
 }
 .head {
@@ -159,6 +187,9 @@ export default {
   background-color: #eeecec;
   border-radius: 10%;
 }
+::v-deep .dizhi {
+  color: black;
+}
 .content2 {
   margin-top: 20px;
   height: 420px;
@@ -190,5 +221,53 @@ export default {
       }
     }
   }
+}
+.comment_list {
+  display: flex;
+
+  .touxiang {
+    flex: 2;
+    text-align: center;
+    img {
+      width: 172px;
+      height: 160px;
+    }
+  }
+  .youall {
+    flex: 8;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    padding-left: 10px;
+    padding-top: 0px;
+    padding-bottom: 0;
+    .nameicon {
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
+.bottomcell {
+  padding: 0;
+}
+.gengduo {
+  font-size: 20px;
+  text-align: center;
+  color: #9f9c9c;
+}
+.skil {
+  float: left;
+  .peoplegrid {
+    font-size: 28px;
+    color: #7885cb;
+    margin: 0 5px;
+    border: solid #7885cb 1px;
+    border-radius: 20px;
+    padding: 10px;
+  }
+}
+.myself {
+  font-size: 18px;
+  color: #a7a2a2;
 }
 </style>
