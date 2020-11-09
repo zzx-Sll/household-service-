e6293cec487374ebd977c7b6e05b77efe4d03e4f
zzdl <2547796262@qq.cn>
2020-11-09 17:04:20 +0800

    优化我的关注页面样式

--- src/views/myfocus/index.vue
+++ src/views/myfocus/index.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="myfocus">
     <!-- 导航栏 -->
-    <van-nav-bar
-      :border="false"
-      class="page-nav-bar"
-      left-text="我的关注"
-      left-arrow
-      @click-left="$router.back()"
-    >
+    <div class="header">
+      <van-nav-bar :border="false"
+                   class="page-nav-bar"
+                   left-text="我的关注"
+                   left-arrow
+                   @click-left="$router.back()">
 
-</van-nav-bar>
-<van-grid direction="horizontal" :column-num="2" :border="false">
-  <van-grid-item  text="家服公司" @click="flag=true" :class="[flag ? 'active' :'']"/>
-  <van-grid-item  text="家服人员" @click="flag=false" :class="[!flag ? 'active' :'']" />
-</van-grid>
+      </van-nav-bar>
+      <van-grid direction="horizontal"
+                :column-num="2"
+                :border="false">
+        <van-grid-item text="家服公司"
+                       @click="flag=true"
+                       :class="[flag ? 'active' :'']" />
+        <van-grid-item text="家服人员"
+                       @click="flag=false"
+                       :class="[!flag ? 'active' :'']" />
+      </van-grid>
+    </div>
 
-<focus-company v-if="flag"></focus-company>
+    <focus-company v-if="flag"></focus-company>
 
-<focus-worker v-else></focus-worker>
+    <focus-worker v-else></focus-worker>
   </div>
 </template>
 
@@ -39,30 +45,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page-nav-bar{
-    height: 128px;
+.myfocus {
+  margin-top: 237px;
+}
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 2;
+}
+.page-nav-bar {
+  height: 128px;
 
-    background-color:#3F51B5 ;
-
-  }
-  ::v-deep .van-grid-item__content{
-    color: white;
-    background-color: #3F51B5;
-
-  }
-  ::v-deep .van-nav-bar__text {
-    color: white;
-    font-size: 32px;
-  }
-  ::v-deep .van-grid-item__text {
-    color: white;
-    font-size: 28px;
-  }
-  ::v-deep .van-icon {
-    color: white;
-  }
-  .active {
-    border-bottom: 3px solid white;
-  }
-
+  background-color: #3f51b5;
+}
+::v-deep .van-grid-item__content {
+  color: white;
+  background-color: #3f51b5;
+}
+::v-deep .van-nav-bar__text {
+  color: white;
+  font-size: 32px;
+}
+::v-deep .van-grid-item__text {
+  color: white;
+  font-size: 28px;
+}
+::v-deep .van-icon {
+  color: white;
+}
+.active {
+  border-bottom: 3px solid white;
+}
 </style>
