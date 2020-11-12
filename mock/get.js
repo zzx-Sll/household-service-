513ca76d0c740759ce669edb85b614095136388e
itheima <916158721@qq.com>
2020-11-12 09:31:32 +0800

    abc

--- mock/get.js
+++ mock/get.js
@@ -24,7 +24,6 @@ export default {
       data: currentCompany
     }
   },
-<<<<<<< HEAD
   'http://localhost:8080/getBusinesstype|get': option => {
     console.log(option)
     // 做筛选出公司的函数
@@ -32,7 +31,8 @@ export default {
       status: 200,
       msg: '请求成功',
       data: [{ tp: '月嫂', active: true }, { tp: '保姆', active: true }, { tp: '育婴师', active: true }, { tp: '产康师', active: true }, { tp: '家装/搬家', active: true }, { tp: '早教/托育', active: true }, { tp: '养老/陪护', active: true }, { tp: '保洁/清洗', active: true }]
-=======
+    }
+  },
   // 获取所有月嫂的接口
   'http://localhost:8080/getAllWorker|get': option => {
     console.log(option)
@@ -52,7 +52,6 @@ export default {
       status: 200,
       msg: '请求成功',
       data: currentWorker
->>>>>>> 6fae86840a0011e83fca9aefb7dcd66b170cafee
     }
   }
 
